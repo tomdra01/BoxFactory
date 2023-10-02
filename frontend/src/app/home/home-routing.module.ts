d69969d7162d30c753a1dd4201aabf7ed0cba363
mrtng1 <mrtngrulyo@gmail.com>
2023-10-02 15:36:09 +0200

    additional files

---
+++ frontend/src/app/home/home-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home.page';
+import {HttpClientModule} from "@angular/common/http";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes), HttpClientModule],
+  exports: [RouterModule]
+})
+export class HomePageRoutingModule {}
