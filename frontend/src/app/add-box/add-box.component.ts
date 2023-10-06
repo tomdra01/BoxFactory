845f759edccc13a0d535f39b0eda4148d2fca048
Tomáš Dračka <tomidracka@gmail.com>
2023-10-06 16:18:45 +0200

    UI Changes, Add box (Needs fix)

---
+++ frontend/src/app/add-box/add-box.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-box',
+  templateUrl: './add-box.component.html',
+  styleUrls: ['./add-box.component.scss'],
+})
+export class AddBoxComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
