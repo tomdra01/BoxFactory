0cef8bd64570811e67bd1b4afb7ef7a76c7c1469
Tomáš Dračka <tomidracka@gmail.com>
2023-10-10 22:25:08 +0200

    New box images

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { State } from 'src/state';
+import {State} from 'src/state';
 import {HttpClient} from "@angular/common/http";
 import {firstValueFrom} from "rxjs";
 import {Box} from "../../models";
@@ -28,18 +28,26 @@ import {environment} from "../../environments/environment";
         </ion-grid>
       </div>
 
-      <!-- Line with cards for different box sizes -->
       <div id="sizeCards" class="page">
         <ion-grid>
           <ion-row>
-            <ion-col *ngFor="let size of ['S', 'M', 'L', 'XL', 'XXL']">
+            <ion-col *ngFor="let size of ['S', 'M', 'L', 'XL']">
               <ion-card>
-                <ion-card-header>
-                  {{size}} Size
-                </ion-card-header>
-                <ion-card-subtitle>Stock: {{ getStockCount(size) }}</ion-card-subtitle>
-                <ion-card-subtitle>Average Price: {{ getAveragePrice(size) }}</ion-card-subtitle>
-                <ion-card-subtitle>Lowest Price: {{ getLowestPrice(size) }}</ion-card-subtitle>
+                <ion-card-content>
+                  <ion-row>
+                    <ion-col class="card-content-left" size="9">
+                      <ion-card-header>
+                        {{ size }} Size
+                      </ion-card-header>
+                      <ion-card-subtitle>Stock: {{ getStockCount(size) }}</ion-card-subtitle>
+                      <ion-card-subtitle>Average Price: {{ getAveragePrice(size) }}</ion-card-subtitle>
+                      <ion-card-subtitle>Lowest Price: {{ getLowestPrice(size) }}</ion-card-subtitle>
+                    </ion-col>
+                    <ion-col class="card-content-right" size="3">
+                      <img class="box-image" *ngIf="size" [src]="getImagePath(size)" alt="Box Image">
+                    </ion-col>
+                  </ion-row>
+                </ion-card-content>
               </ion-card>
             </ion-col>
           </ion-row>
@@ -81,7 +89,6 @@ import {environment} from "../../environments/environment";
 })
 
 export class HomePage implements OnInit {
-  //filter
   public sizeFilters: { [key: string]: boolean } = {
     'S': true,
     'M': true,
@@ -89,15 +96,24 @@ export class HomePage implements OnInit {
     'XL': true,
     'XXL': true
   };
+
   public filteredBoxes: Box[] = [];
+
+  currentImageIndex: number = 0;
+
   public applyFilter(): void {
     this.filteredBoxes = this.state.boxes.filter(box => box.size !== undefined && this.sizeFilters[box.size.toString() as keyof typeof this.sizeFilters]);
   }
 
   constructor(private http: HttpClient, public state: State) {}
+
   async ngOnInit(): Promise<void> {
     await this.fetchBoxes();
     this.applyFilter();
+
+    setInterval(() => {
+      this.currentImageIndex++;
+    }, 15000);
   }
 
   async fetchBoxes(): Promise<void> {
@@ -165,5 +181,26 @@ export class HomePage implements OnInit {
 
     const lowest = Math.min(...boxesOfSize.map(box => box.price || Infinity));  // box.price || Infinity ensures it falls back to Infinity if undefined
     return lowest.toFixed(2);
+  }
+
+  getImagePath(size: string): string {
+    const sizeMappings: { [key: string]: string } = {
+      'XL': 'box-XL-right.png',
+      'L': 'box-L-right.png',
+      'M': 'box-M-right.png',
+      'S': 'box-S-right.png'
+    };
+
+    const leftImageMappings: { [key: string]: string } = {
+      'XL': 'box-XL-left.png',
+      'L': 'box-L-left.png',
+      'M': 'box-M-left.png',
+      'S': 'box-S-left.png'
+    };
+
+    const isRightImage = this.currentImageIndex % 2 === 0;
+    const mappings = isRightImage ? sizeMappings : leftImageMappings;
+
+    return `/assets/images/boxes/${mappings[size] || ''}`;
   }
 }
